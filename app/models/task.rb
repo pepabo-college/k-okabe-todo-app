032eb22888846d5c17b0e323d9a86550ffde3aff
k.okabe <k.okabe@pmac451s.pepabo.private>
2017-06-05 14:48:50 +0900

    taskモデルのクラス名を修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
-class Tasks < ActiveRecord::Base
-  enum status: { todo: 0, doing: 1, done:2 }
+class Task < ActiveRecord::Base
+  enum status: { todo: 0, doing: 1, done: 2 }
   validates :content, presence: true
 end
