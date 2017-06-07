fcd1047a11c822f9f538b8b3feea44eeba9854dc
k.okabe <k.okabe@pmac451s.pepabo.private>
2017-06-07 17:11:41 +0900

    テストコードshoulda-matchersの形式に変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-  describe 'content' do
-    context '空白のとき' do
-      it 'valid でないこと' do
-        task = Task.new(content: nil)
-        task.valid?
-        expect(task.errors[:content]).to be_present
-      end
-    end
-  end
+  it { is_expected.to validate_presence_of(:content) }
 end
