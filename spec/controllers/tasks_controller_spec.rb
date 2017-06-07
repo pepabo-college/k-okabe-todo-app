8832e388bb1f28b3d78564a40b9ac11c882589b6
k.okabe <k.okabe@pmac451s.pepabo.private>
2017-06-07 17:45:44 +0900

    new edit destroyのテストケースを追加

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
+  describe 'GET #new' do
+    context 'TODOが作成された時' do
+      it 'showに遷移されること' do
+        expect(response).to redirect_to(task_path)
+      end
+    end
+  end
 
+  describe 'GET #edit' do
+    context 'TODOが更新された時' do
+      it 'showに遷移されること' do
+        expect(response).to redirect_to(task_path)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    context 'TODOが削除された時' do
+      it 'indexに遷移されること' do
+        expect(response).to redirect_to(tasks_path)
+      end
+    end
+  end
 end
