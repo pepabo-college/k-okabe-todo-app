763abc35cfedb118b23e1a19a8343e41ecc4ee5f
k.okabe <k.okabe@pmac451s.pepabo.private>
2017-06-13 17:54:08 +0900

    [WIP]task#showのテストコードを追加

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,27 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe TasksController, type: :controller do
-  describe 'GET #new' do
-    context 'TODOが作成された時' do
-      it 'showに遷移されること' do
-        expect(response).to redirect_to(task_path)
-      end
+describe TasksController do
+  describe 'GET #show' do
+    it "assings the requested task to @task" do
+      task = create(:task)
+      get :show, id: task
+      expect(assings(:task)).to eq task
     end
-  end
 
-  describe 'GET #edit' do
-    context 'TODOが更新された時' do
-      it 'showに遷移されること' do
-        expect(response).to redirect_to(task_path)
-      end
-    end
-  end
-
-  describe 'DELETE #destroy' do
-    context 'TODOが削除された時' do
-      it 'indexに遷移されること' do
-        expect(response).to redirect_to(tasks_path)
-      end
+    it "renders the :show template" do
+      task = create(:task)
+      get :show, id: task
+      expect(response).to render_template :show
     end
   end
 end
