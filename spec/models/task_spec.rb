003012257513f3415f702205661309cd1e3b4024
k.okabe <k.okabe@pmac451s.pepabo.private>
2017-06-13 14:57:55 +0900

    taskのテストコードを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
+describe Task do
+  it "has a valid factory" do
+    expect(build(:task)).to be_valid
+  end
   it { is_expected.to validate_presence_of(:content) }
 end
