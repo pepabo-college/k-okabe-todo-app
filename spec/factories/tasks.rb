648f2692e7242c9c9121b4aec1178259d8baf8fb
k.okabe <k.okabe@pmac451s.pepabo.private>
2017-06-23 09:45:47 +0900

    POSTのテスト用Factoryを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,5 +2,15 @@ FactoryGirl.define do
   factory :task do
     sequence(:content){|n| "#{n}:テストTODO"}
     status [*0..2].sample
+
+    factory :new_task do
+      status { ['todo', 'doing', 'done'].sample }
+    end
+
+    factory :invalid_task do
+      content nil
+      status { ['todo', 'doing', 'done'].sample }
+    end
+
   end
 end
